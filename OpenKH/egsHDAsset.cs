1921402242ca7073ef1b11debf5e30eb40c61cde
AntonioDePau <antoine301191@hotmail.fr>
2021-05-01 12:26:03 +0100

    Try with System.IO.Compression

--- OpenKH/egsHDAsset.cs
+++ OpenKH/egsHDAsset.cs
@@ -2,8 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-//using System.IO.Compression;
-using Ionic.Zlib;
+using System.IO.Compression;
+//using Ionic.Zlib;
 using System.Linq;
 using Xe.BinaryMapper;
 
