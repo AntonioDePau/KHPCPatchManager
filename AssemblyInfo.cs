0a265480358585f75dd80a1dd5b8c8e7926e14a7
AntonioDePau <antoine301191@hotmail.fr>
2021-05-01 08:39:18 +0100

    updated version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyFileVersion("0.0.7")]
+[assembly: AssemblyFileVersion("0.0.9")]
